4b096160894f13c61eb283ce3bd20469692ad8b3
HP <HP@DESKTOP-FAKO79N>
2024-05-10 13:03:09 +0530

    Third Commit

---
+++ AibProject1/src/main/java/com/aditya/controller/LoginOperationController.java
@@ -0,0 +1,47 @@
+package com.aditya.controller;
+
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginOperationController {
+	
+	
+	
+	@GetMapping("/")
+	public String getHomePage() 
+	{
+		
+		return "home";
+	}
+	
+	
+	@GetMapping("/addhighlights")
+	public String get() 
+	{
+		
+		return "addhighlights";
+		
+	}
+	
+	@GetMapping("/showhighlights")
+	public String show() 
+	{
+		
+		return "showhighlight";
+		
+	}
+	
+	
+	@GetMapping("/denied")
+	public String access_denied() 
+	{
+		return "access_denied";
+	
+	}
+	
+
+}
